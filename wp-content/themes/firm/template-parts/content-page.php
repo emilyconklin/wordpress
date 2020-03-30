a872a4baa3d9763f06c54ab00c072bcbd77c1421
Emily Conklin <emilyconklin31@gmail.com>
2020-03-30 15:36:55 -0600

    first commit

---
+++ wp-content/themes/firm/template-parts/content-page.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package firm
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php firm_post_thumbnail(); ?>
+
+	<div class="entry-content">
+		<?php
+		the_content();
+
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'firm' ),
+			'after'  => '</div>',
+		) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php if ( get_edit_post_link() ) : ?>
+		
+	<?php endif; ?>
+</article><!-- #post-<?php the_ID(); ?> -->
